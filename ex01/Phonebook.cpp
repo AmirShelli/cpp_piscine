b1fd69b5e4943a1fd757b83f618e89626a85a8a2
iriska <thebluemitsu@gmail.com>
2022-03-16 19:49:42 +0300

    save

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -1,5 +1,14 @@
-#include <string>
+#include <string.h>
 #include <iostream>
+#include <cstdlib>
+
+
+void myGetline(std::string *input)
+{
+	getline(std::cin, *input);
+	if(*input == "EXIT" || *input == "exit")
+		exit(1);
+}
 
 struct info
 {
@@ -45,20 +54,27 @@ class PhoneBook{
 	private:
 		Contact 	MyContacts[9];
 		static int	numberOfContacts;
-		void fillInfo(char const *message)
+		std::string fillInfo(char const *message)
 		{	
 			int flag;
 			std::string input;
 			flag = 1;
-			
 			while(flag)
 			{
 				std::cout << "contact's " << message << ": ";
-				getline(std::cin, input);
-				if(flag = input.empty())
+				myGetline(&input);
+				if((flag = input.empty()))
 					std::cout << "contact's " << message 
 						<< "is empty, please fill in the field." << std::endl;
 			}
+			return (input);
+		}
+		void displayContact(int index)
+		{
+			std::cout << index + 1 << " | "
+				<< MyContacts[index].getFirstName() << " | "
+				<< MyContacts[index].getLastName() << " | "
+				<< MyContacts[index].getNickname() << std::endl;
 		}
 	public:
 		void add()
@@ -66,11 +82,11 @@ class PhoneBook{
 			info args;
 			int i;
 
-			args.firstName		= ("first name");
-			args.lastName		= ("last name");
-			args.nickname		= ("nickname");
-			args.phoneNumber	= ("phone number");
-			args.secret 		= ("secret");
+			args.firstName		= fillInfo("first name");
+			args.lastName		= fillInfo("last name");
+			args.nickname		= fillInfo("nickname");
+			args.phoneNumber	= fillInfo("phone number");
+			args.secret 		= fillInfo("secret");
 			i = 0;
 			while(!(MyContacts[i].getFirstName().empty())
 				&& i < 9)
@@ -79,6 +95,42 @@ class PhoneBook{
 		}
 		void search()
 		{
+			int i;
 
+			for (i = 0; i < 8; i++)
+				if(!MyContacts[i].getFirstName().empty())
+					displayContact(i);
+			{
+				if(i > 0)
+				{
+					std::cout << "please provide a valid index to display: ";
+					std::cin >> i;
+				} 
+				else
+					std::cout << "no contacts to show.\n"; 
+			} while(i - 1 >= 0 || i - 1 <= 7);
+			displayContact(i - 1); 
+			// will show anyway??? 
+			// error in indexing, set another limit
 		}
 };
+
+int main()
+{
+	std::string input;
+	PhoneBook sample;
+
+	while(1)
+	{
+		std::cout << "\nwhat would you like to do?\n"
+		<< "\tADD\tSEARCH\tEXIT\n";
+		myGetline(&input);
+		if(input == "SEARCH" || input == "search")
+			sample.search();
+		else if((input == "add" || input == "add"))
+				sample.add();
+		else
+			std::cout << "sorry, no such option.\n";
+
+	}
+}
