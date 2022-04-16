58d490884e67634a180a5639c2916e6ae857adb9
iriska <thebluemitsu@gmail.com>
2022-04-17 01:28:26 +0300

    save

--- 04/ex01/src/main.cpp
+++ 04/ex01/src/main.cpp
@@ -8,18 +8,32 @@ int main()
 	std::cout << "\t### Animal ###"<<std::endl;
 
     // const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    std::cout << j->getType() << ": ";
-	j->makeSound();
-    std::cout << i->getType() << ": ";
-	i->makeSound();
+    // const Animal* j = new Dog();
+    // const Animal* i = new Cat();
+    // std::cout << j->getType() << ": ";
+	// j->makeSound();
+    // std::cout << i->getType() << ": ";
+	// i->makeSound();
     // will output the cat sound!
     // meta->makeSound();
     
     // delete meta;
-    delete j;
-    delete i;
+    // delete j;
+    // delete i;
+
+	Animal *m[10];
+    for (int i = 0; i < 10; i++)
+    {
+        if (i < 5)
+            m[i] = new Dog();
+        else
+            m[i] = new Cat();
+    }
+    for (int j = 0; j < 10; j++)
+    {
+        delete m[j];
+    }
+    
 
 	return 0;
 }
