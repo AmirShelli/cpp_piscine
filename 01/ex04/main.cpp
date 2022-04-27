0683f6d0cfad130ed297d710ddec0eaa4cbc7484
Boyd Harghaz <bharghaz@pl-b1.msk.21-school.ru>
2022-04-27 17:32:01 +0300

    save

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -20,24 +20,38 @@ int main(int ac, char *av[])
 	std::ofstream fout;
 	std::ifstream fin;
 	std::string line;
-	char *file = strdup(av[1]);
 
 	if(ac == 4 && std::string(av[2]) != "" && std::string(av[3]) != "\0")
 	{
+		char *file = strdup(av[1]);
+
 		fin.open(file);
-		fout.open(std::strcat(file , ".replace"));	
+		if (!fin.is_open())
+		{
+			std::cout << "can't oepn fin" << std::endl;
+			delete file;
+			return (-1);
+		}
+
+		fout.open(std::strcat(file , ".replace"));
+		if (!fout.is_open())
+		{
+			std::cout << "can't open fout" << std::endl;
+			fin.close();
+			delete file;
+			return (-1);
+		}
 		while(getline(fin, line))
 		{	
 			replace(line, av[2], av[3]);
 			fout << line << std::endl;
 		}
-		delete file;
 		fin.close();
 		fout.close();
+
+		delete file;
 	}
-	else 
-	{	
-		std::cout << "not enough arguments" << std::endl; 
-		return (1);
-	}
+	else
+		std::cout << "wrong arguments!" << std::endl;
+	
 }
